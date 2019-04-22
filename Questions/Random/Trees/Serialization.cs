cbd3e28a216b4b2e0f62ad0dff792e9d7d9c6428
Mohannad Abwah <2223788+mohannad-abwah@users.noreply.github.com>
2019-04-21 23:58:12 -0700

    Added TreeNode class & Deserialize tests

---
+++ Questions/Random/Trees/Serialization.cs
@@ -0,0 +1,100 @@
+﻿namespace Trees
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Deserialization
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(TestData))]
+        public void SerializeDeserialize(TreeNode root)
+        {
+	        string serializedTree = root.ToString();
+            TreeNode deserializedTree = Deserialize(serializedTree);
+	        Assert.AreEqual(root, deserializedTree);
+        }
+
+        public static TreeNode Deserialize(string serializedTree) {
+            Queue queue = new Queue(serializedTree.Split(','));
+            return Deserialize(queue);
+        }
+
+        private static TreeNode Deserialize(Queue q)
+        {
+            string item = (string) q.Dequeue();
+            if (item == "#")
+                return null;
+        
+            TreeNode node = new TreeNode{ Value = int.Parse(item) };
+            node.Left = Deserialize(q);
+            node.Right = Deserialize(q);
+            return node;
+        }
+
+        public static IEnumerable<object[]> TestData =>
+            new[]
+            {
+                new object[]
+                {
+                    new TreeNode
+                    {
+                        Value = 2,
+                        Left = new TreeNode
+                        {
+                            Value = 1
+                        },
+                        Right = new TreeNode
+                        {
+                            Value = 3
+                        }
+                    }
+                },
+                new object[]
+                {
+                    new TreeNode
+                    {
+                        Value = 2,
+                        Left = new TreeNode
+                        {
+                            Value = 1
+                        }
+                    }
+                },
+                new object[]
+                {
+                    new TreeNode
+                    {
+                        Value = 2,
+                        Right = new TreeNode
+                        {
+                            Value = 3
+                        }
+                    }
+                },
+                new object[]
+                {
+                    new TreeNode
+                    {
+                        Value = 2,
+                        Left = new TreeNode
+                        {
+                            Value = 1,
+                            Left = new TreeNode
+                            {
+                                Value = 3
+                            }
+                        }
+                    }
+                },
+                new object[]
+                {
+                    new TreeNode
+                    {
+                        Value = 2
+                    }
+                }
+            };
+    }
+}
---
+++ Questions/Random/Trees/TreeNode.cs
@@ -0,0 +1,64 @@
+﻿namespace Trees
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class TreeNode : IEquatable<TreeNode>
+    {
+	    public int Value { get; set; }
+
+	    public TreeNode Left { get; set; }
+
+	    public TreeNode Right { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is TreeNode other)
+            {
+                return this.Value == other.Value && this.Left == other.Left && this.Right == other.Right;
+            }
+
+            return false;
+        }
+
+        public bool Equals(TreeNode other)
+        {
+            return other != null &&
+                    Value == other.Value &&
+                    EqualityComparer<TreeNode>.Default.Equals(Left, other.Left) &&
+                    EqualityComparer<TreeNode>.Default.Equals(Right, other.Right);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Value, Left, Right);
+        }
+            
+        public override string ToString()
+        {
+            return Serialize(this);
+        }
+
+        #region Operators
+
+        public static bool operator ==(TreeNode node1, TreeNode node2)
+        {
+            return EqualityComparer<TreeNode>.Default.Equals(node1, node2);
+        }
+
+        public static bool operator !=(TreeNode node1, TreeNode node2)
+        {
+            return !(node1 == node2);
+        }
+
+        private static string Serialize(TreeNode root)
+        {
+            if (root == null)
+                return "#";
+        
+            return $"{root.Value},{Serialize(root.Left)},{Serialize(root.Right)}";
+        }
+
+        #endregion
+    }
+}
